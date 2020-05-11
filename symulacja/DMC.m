1abca4411aed328ed5479657eed7b8d267ebb571
Michal <szpunar98@gmail.com>
2020-05-11 20:51:08 +0200

    DMC written, not tested (probably couple hours of debugging, whichc cheers me up)

--- symulacja/DMC.m
+++ symulacja/DMC.m
@@ -1,22 +1,34 @@
 classdef DMC
     
     properties
-        M  % wymiary (N*ny)x(Nu*nu)
-        Mp  % wymiary (N*ny)x((D-1)*nu)
-        K  % wymiary (Nu*nu)x(N*ny)
+        M  % dim (N*ny)x(Nu*nu)
+        Mp  % dim (N*ny)x((D-1)*nu)
+        K  % dim (Nu*nu)x(N*ny)
+        K1 % dim nu x(N*ny)
+        
+        D %scalar
+        N %scalar
+        Nu %scalar
     end
     
     methods
-        function obj = setup(D, N, Nu, S_y1u1, S_y2u1, S_y1u2, S_y2u2)
+        function obj = DMC(D, N, Nu, lambda, S_y1u1, S_y2u1, S_y1u2, S_y2u2)
             % constructing the S components
             S_arr = {};
             for i =1:D
-                S_arr{i} = [S_y1u1(i),S_y1u2(i); 
+                S_arr{end+1} = [S_y1u1(i),S_y1u2(i); 
                             S_y2u1(i),S_y2u2(i)];
             end
             
             % constructing the dynamic matrix
             obj.M = cell(N,Nu);
+            zero_cell = {zeros(2)};
+            for i = 1:size(obj.M,1)
+                for j = 1:size(obj.M,2)
+                    obj.M(i,j) = zero_cell;
+                end
+            end
+            
             for i = 1:size(obj.M,1)
                 for j = 1:size(obj.M,2)
                     if i>=j
@@ -26,27 +38,65 @@ classdef DMC
             end
             obj.M = cell2mat(obj.M);
             
-            % constructing the Mp matrix
+            % constructing the Mp matrix        
             obj.Mp = cell(N,D-1);
             for i = 1:size(obj.Mp,1)
                 for j = 1:size(obj.Mp,2)
+                    obj.Mp(i,j) = zero_cell;
+                end
+            end
+            
+            for i = 1:size(obj.Mp,1)
+                for j = 1:size(obj.Mp,2)
                     if i+j<D
-                        obj.Mp(i,j) = S_arr(i+j) - S_arr(j);
+                        obj.Mp(i,j) = {cell2mat(S_arr(i+j)) - cell2mat(S_arr(j))};
                     else
-                        obj.Mp(i,j) = S_arr(D) - S_arr(j);
+                        obj.Mp(i,j) = {cell2mat(S_arr(D)) - cell2mat(S_arr(j))};
                     end
                 end
             end
             obj.Mp = cell2mat(obj.Mp);
             
-            obj.K = ((obj.M'*obj.M + lambda*eye(Nu))^-1)*obj.M';
+            obj.K = ((obj.M'*obj.M + lambda*eye(Nu*2))^-1)*obj.M';
+            obj.K1 = obj.K(1:2, :);
             
+            obj.D = D;
+            obj.N = N;
+            obj.Nu = Nu;
         end
         
         
-        %TODO
-        function controls = eval_control_vals(obj,inputArg)
+        
+        function err = regulate(obj, sim_time, Yzad, init_controls) %Yzad = sim_time vector x ny
+            nu = 2;
+            ny = 2;
             
+            err = zeros(ny,1);
+            
+            dUp = zeros((obj.D - 1)*nu);
+            controls = init_controls;
+            
+            for k = 1:sim_time
+                % reading input
+                mrm = readMeasurements([1,3]);
+                
+                e = mrm' - Yzad(k,:)';
+                err = err + e.^2;
+                
+                % creating Y(k) and Yzad(k)
+                Y = repmat(mrm', obj.N, 1);
+                Yzad_k = repmat(Yzad(k,:)', obj.N, 1);
+                
+                % evaluating delta u
+                du = obj.K1*(Yzad_k - Y - obj.Mp * dUp);
+                
+                % sending controls
+                controls = controls + du';
+                sendControls([5,6]    ,... send for these elements
+                             controls);  % new corresponding control values
+                
+                
+            end
         end
     end
 end
